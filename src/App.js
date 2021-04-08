20617eceb44b7fab5409a4ed8fc23d1bf3d2c920
Samy0412 <samantha.gadet@gmail.com>
2021-04-07 20:31:41 -0400

    style for mobile

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   const audioRef = useRef(null);
 
   return (
-    <div className="App">
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
       <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song currentSong={currentSong} />
       <Player
